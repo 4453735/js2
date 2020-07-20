e0b1b999f5b3feacf23bf88c3636da81e7f985ae
Tim K <4453735@gmail.com>
2020-07-20 15:38:22 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const products = [
+    {id: 1, title: 'Notebook', price: 20000},
+    {id: 2, title: 'Mouse', price: 1500},
+    {id: 3, title: 'Keyboard', price: 5000},
+    {id: 4, title: 'Gamepad', price: 4500},
+];
+
+const renderProduct = (item, img ='./img/no-image.png') =>
+    `<div class="product-item card col">
+                <img src="${img}" alt="image" class="product-img">
+                <h3 class="product-title">${item.title}</h3>
+                <p class="product-price">${item.price} руб</p>
+                <button class="by-btn">В корзину</button>
+            </div>`;
+
+const renderProducts = list => {
+    document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item, item.img)).join(''));
+};
+
+renderProducts(products);
