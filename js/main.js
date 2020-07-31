aaf7587a7f355acdcb011386824c256bd07c630d
Tim K <4453735@gmail.com>
2020-07-31 15:57:55 +0300

    Lesson 2

--- js/main.js
+++ js/main.js
@@ -1,23 +1,102 @@
-'use strict';
+class ProductList {
+  #privateProp;
 
-const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
-];
+  constructor(container = '.products') {
+    this.container = container;
+    this.goods = [];
+    this.allProducts = [];
+    this.#privateProp = '123';
 
-const renderProduct = (item, img ='./img/no-image.png') =>
-    `<div class="product-item card col">
-                <img src="${img}" alt="image" class="product-img">
-                <h3 class="product-title">${item.title}</h3>
-                <p class="product-price">${item.price} руб</p>
-                <button class="by-btn">В корзину</button>
-            </div>`;
+    this.#fetchProducts();
+    this.render();
+    this.totalProductsPrice();
+  }
 
-const renderProducts = list => {
-    document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item, item.img)).join(''));
-};
+  get prop() {
+    return this.#privateProp;
+  }
+
+  set prop(value) {
+    this.#privateProp = value;
+  }
+
+  #fetchProducts() {
+    this.goods = [
+      {id: 1, title: 'Notebook', price: 20000},
+      {id: 2, title: 'Mouse', price: 1500},
+      {id: 3, title: 'Keyboard', price: 5000},
+      {id: 4, title: 'Gamepad', price: 4500},
+    ];
+  }
+
+  render() {
+    const block = document.querySelector(this.container);
+
+    for (let product of this.goods) {
+      const productObject = new ProductItem(product);
+
+      this.allProducts.push(productObject);
+      block.insertAdjacentHTML('beforeend', productObject.render());
+    }
+  }
+
+  // Мое решение
+  totalProductsPrice(){
+    let sum = 0;
+    this.goods.forEach(({price}) => {
+      sum += price;
+    });
+    console.log(`Суммарная стоимость всех товаров: ${sum} руб.`);
+  }
+
+  calcSum() {
+    return this.allProducts.reduce((sum, {price}) => sum + price, 0)
+  }
+}
+
+class ProductItem {
+  constructor(product, img = 'https://placehold.it/200x150') {
+    this.title = product.title;
+    this.price = product.price;
+    this.id = product.id;
+    this.img = img;
+  }
+
+  render() {
+    return `<div class="product-item" data-id="${this.id}">
+              <img src="${this.img}" alt="Some img">
+              <div class="desc">
+                  <h3>${this.title}</h3>
+                  <p>${this.price} \u20bd</p>
+                  <button class="buy-btn">Купить</button>
+              </div>
+          </div>`;
+  }
+}
+
+const list = new ProductList();
+console.log(list.calcSum());
 
 
-renderProducts(products);
+// const products = [
+//   {id: 1, title: 'Notebook', price: 20000},
+//   {id: 2, title: 'Mouse', price: 1500},
+//   {id: 3, title: 'Keyboard', price: 5000},
+//   {id: 4, title: 'Gamepad', price: 4500},
+// ];
+//
+// const renderProduct = (item, img='https://placehold.it/200x150') => `<div class="product-item" data-id="${this.id}">
+//               <img src="${img}" alt="Some img">
+//               <div class="desc">
+//                   <h3>${item.title}</h3>
+//                   <p>${item.price} \u20bd</p>
+//                   <button class="buy-btn">Купить</button>
+//               </div>
+//           </div>`;
+//
+// const renderProducts = list => {
+// document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(''));
+// };
+//
+// renderProducts(products);
+//
